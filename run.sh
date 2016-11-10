1473718ac228f333d0e973b751bf52ff24ac2171
Brian Mock <brian@mockbrian.com>
2016-11-09 20:31:00 -0800

    Rough idea is complete

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 pebble build
-pebble install --emulator basalt
+# pebble install --emulator basalt
+pebble install --phone 10.1.1.102
--- src/c/pebble-watchface.c
+++ src/c/pebble-watchface.c
@@ -1,21 +1,27 @@
 #include <pebble.h>
 
+// TODO: Switch to TextLayers rather than drawing text manually.
+// https://developer.pebble.com/docs/c/User_Interface/Layers/TextLayer/
+
 static Window *s_main_window = 0;
 static Layer *s_canvas_layer = 0;
 static struct tm *time_now = 0;
 static BatteryChargeState battery_charge;
 
-static GColor8 color_text;
+static GColor8 color_time;
+static GColor8 color_date;
 static GColor8 color_batt;
 static GColor8 color_steps;
 static GColor8 color_bg;
 
 static int hours = 0;
 static int minutes = 0;
+static int month = 1;
+static int date = 1;
 static int step_count = -1;
 
-static char time_str[255];
-static char step_str[255];
+const char *EMOJI_PUCKERED = "\U0001f617";
+const char *EMOJI_SMILE = "\U0001f60a";
 
 // static bool
 // step_data_is_available() {
@@ -48,6 +54,8 @@ static void
 tick_handler(struct tm *tick_time, TimeUnits _changed) {
   hours = tick_time->tm_hour;
   minutes = tick_time->tm_min;
+  date = tick_time->tm_mday;
+  month = tick_time->tm_mon + 1;
   if (minutes == 0) {
     vibes_short_pulse();
   }
@@ -60,30 +68,74 @@ battery_handler(BatteryChargeState c) {
   safe_mark_dirty(s_canvas_layer);
 }
 
+// static void
+// draw_battery_line(GRect bounds, Layer *layer, GContext *ctx) {
+//   uint8_t percent = battery_charge.charge_percent;
+//   // TODO: Handle .is_charging
+//   // TODO: Handle .is_plugged
+//   const int x = 0;
+//   const int y = bounds.size.h - 10;
+//   const int w =(bounds.size.w * percent) / 100;
+//   const int h = 10; 
+//   GRect frame = GRect(x, y, w, h);
+//   graphics_context_set_fill_color(ctx, color_batt);
+//   graphics_fill_rect(ctx, frame, 0, GCornerNone);
+// }
+
 static void
-draw_battery(GRect bounds, Layer *layer, GContext *ctx) {
+draw_battery_chunks(GRect bounds, Layer *layer, GContext *ctx) {
+  const int gap = 3;
+  const int radius = 4;
   uint8_t percent = battery_charge.charge_percent;
-  // TODO: Handle .is_charging
-  // TODO: Handle .is_plugged
-  const int x = 0;
-  const int y = bounds.size.h - 10;
-  const int w =(bounds.size.w * percent) / 100;
-  const int h = 10; 
-  GRect frame = GRect(x, y, w, h);
-  graphics_context_set_fill_color(ctx, color_batt);
-  graphics_fill_rect(ctx, frame, 0, GCornerNone);
+  const int n = percent / 10;
+  for (int i = 0; i < 10; i++) {
+    graphics_context_set_stroke_width(ctx, 2);
+    graphics_context_set_stroke_color(ctx, color_batt);
+    graphics_context_set_fill_color(ctx, color_batt);
+    const GPoint point = {
+      radius + 2 * (radius + gap) * i,
+      bounds.size.h - 10
+    };
+    if (i <= n) {
+      graphics_draw_circle(ctx, point, radius);
+      graphics_fill_circle(ctx, point, radius);
+    } else {
+      graphics_draw_circle(ctx, point, radius);
+    }
+  }
 }
 
 static void
 draw_time(GRect bounds, Layer *layer, GContext *ctx) {
-  GRect frame = GRect(0, 0, bounds.size.w, 50);
-  snprintf(time_str, sizeof time_str, "%02d:%02d", hours, minutes);
-  graphics_context_set_text_color(ctx, color_text);
+  GRect frame = GRect(0, 50, bounds.size.w, 50);
+  char str[255];
+  snprintf(str, sizeof str, "%02d:%02d", hours, minutes);
+  GColor text_color = gcolor_legible_over(color_time);
+  graphics_context_set_text_color(ctx, text_color);
+  graphics_context_set_fill_color(ctx, color_time);
+  graphics_fill_rect(ctx, frame, 0, GCornerNone);
   graphics_draw_text(
     ctx,
-    time_str,
+    str,
     fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
     // fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49),
+    frame,
+    GTextOverflowModeTrailingEllipsis,
+    GTextAlignmentCenter,
+    NULL
+  );
+}
+
+static void
+draw_date(GRect bounds, Layer *layer, GContext *ctx) {
+  GRect frame = GRect(0, 10, bounds.size.w, 50);
+  char str[255];
+  snprintf(str, sizeof str, "%02d/%02d", month, date);
+  graphics_context_set_text_color(ctx, color_date);
+  graphics_draw_text(
+    ctx,
+    str,
+    fonts_get_system_font(FONT_KEY_LECO_32_BOLD_NUMBERS),
     frame,
     GTextOverflowModeTrailingEllipsis,
     GTextAlignmentCenter,
@@ -96,13 +148,16 @@ draw_steps(GRect bounds, Layer *layer, GContext *ctx) {
   if (step_count == -1) {
     return;
   }
-  GRect frame = GRect(0, 60, bounds.size.w, 50);
-  snprintf(step_str, sizeof step_str, "%d", step_count);
+  GRect frame = GRect(0, 100, bounds.size.w, 50);
+  const char *emoji = EMOJI_SMILE;
+  char str[255];
+  // snprintf(str, sizeof str, "%d %s", step_count, emoji);
+  snprintf(str, sizeof str, "%d", step_count);
   graphics_context_set_text_color(ctx, color_steps);
   graphics_draw_text(
     ctx,
-    step_str,
-    fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS),
+    str,
+    fonts_get_system_font(FONT_KEY_LECO_32_BOLD_NUMBERS),
     frame,
     GTextOverflowModeTrailingEllipsis,
     GTextAlignmentCenter,
@@ -115,8 +170,9 @@ update_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
   graphics_context_set_antialiased(ctx, true);
   draw_time(bounds, layer, ctx);
+  draw_date(bounds, layer, ctx);
   draw_steps(bounds, layer, ctx);
-  draw_battery(bounds, layer, ctx);
+  draw_battery_chunks(bounds, layer, ctx);
 }
 
 static void
@@ -168,7 +224,8 @@ init_battery() {
 static void
 init() {
   srand(time(NULL));
-  color_text = GColorWhite;
+  color_time = GColorWhite;
+  color_date = GColorRichBrilliantLavender;
   color_batt = GColorRajah;
   color_steps = GColorMelon;
   color_bg = GColorBlack;
